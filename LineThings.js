db3b6e399766a2d2b9ce08cabe65583c41bd86e2
ilantzou <30565373+ilantzou@users.noreply.github.com>
2019-06-06 06:07:48 +0800

    Update LineThings.js

--- LineThings.js
+++ LineThings.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '627eec74-c73b-4122-86eb-8357c33ee8ae'; // LED, Button
+const USER_SERVICE_UUID         = 'd67a1802-c3cd-4a8f-9064-9d55fd7ffe64'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
